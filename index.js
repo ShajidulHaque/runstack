f02bbf92f97f4649ed4c78de71df23aae2f50ada
Shahidul Haque Shihab <77093976+shajidulhaqueshihab@users.noreply.github.com>
2021-11-24 07:25:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send(`<!DOCTYPE html>
-		    	<html>
-		    	
-		    	<head>
-		    	    <meta charset="UTF-8">
-		    	    <meta name="viewport" content="width=device-width, initial-scale=1">
-		    	    <title>run stack</title>
-		    	</head>
-		    	
-		    	<body style="margin: 0; padding: 0; color: #333; text-align: center; font-family: Arial, Helvetica, sans-serif">
-		    		<h1>Run Stack</h1>
-		    		<h3>We are coming soon....</h3>
-		    		<h5>😀😀😀</h5>
-		    	</body>
-		    	
-		    	</html>`);
-})
-
+    res.send('hi');
+});
 app.listen(3000, () => {});
